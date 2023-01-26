851c8d90f88d6912471e8679c8e041f2c14dc659
Gabriel Kenji <fh178@t-systems.com.br>
2023-01-26 19:49:53 -0300

    [FEAT]: Adicionando Service

---
+++ TorneioFinal/Business/Interfaces/IParticipanteService.cs
@@ -0,0 +1,14 @@
+﻿using Infra.Object;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    public interface IParticipanteService
+    {
+        public List<ParticipanteModel> ListandoParticipantes();
+    }
+}
---
+++ TorneioFinal/Business/Services/ParticipanteService.cs
@@ -0,0 +1,33 @@
+﻿using Business.Interfaces;
+using Infra.Interfaces;
+using Infra.Object;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services
+{
+    public class ParticipanteService : IParticipanteService
+    {
+        private readonly IParticipanteRepository _participanteRepository;
+
+        public ParticipanteService(IParticipanteRepository participanteRepository)
+        {
+            _participanteRepository = participanteRepository;
+        }
+        public List<ParticipanteModel> ListandoParticipantes()
+        {
+            try
+            {
+                return _participanteRepository.ListarParticipantes();
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception("Erro ao listar os itens " + e.Message);
+            }
+        }
+    }
+}
