e1009d45831814d9d8343f8387776665e9bad481
Gabriel Kenji <fh178@t-systems.com.br>
2023-01-26 18:51:03 -0300

    [Feat]: Estruturando os projetos

--- TorneioFinal/Presentation/Program.cs
+++ TorneioFinal/Presentation/Program.cs
@@ -1,7 +1,13 @@
+using Infra.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BancoContext>
+    (options => options.UseSqlServer(builder.Configuration["ConnectionStrings:DataBase"]));
 
 var app = builder.Build();
 
